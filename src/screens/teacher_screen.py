d00e1067de387b49e40e10833ebcfc8031ed321e
AnjaliSonavane10 <anjalisonavane10@gmail.com>
2026-09-16 16:53:01 +0530

    teacher subject manage

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -4,8 +4,8 @@ from src.ui.base_layout import style_base_dashboard,style_base_layout
 from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
 
-from src.database.db import check_teacher_exists,create_teacher,teacher_login
-
+from src.database.db import check_teacher_exists,create_teacher,teacher_login,get_teacher_subjects
+from src.components.dialog_create_subject import create_subject_dialog
 def teacher_screen():
     style_base_dashboard()
     style_base_layout()
@@ -75,7 +75,37 @@ def teacher_tab_take_attendance():
     st.header('Take AI Attendance')
 
 def teacher_tab_manage_subjects():
-    st.header('Manage Subjects')
+    teacher_id=st.session_state.teacher_data['teacher_id']
+    col1,col2=st.columns(2)
+    with col1:
+        st.header('Manage Subjects',width='stretch')
+    with col2:
+        if st.button('Create New subject',width='stretch'):
+            create_subject_dialog(teacher_id)
+
+    #List all subjects
+    subjects = get_teacher_subjects(teacher_id)
+    if subjects:
+        for sub in subjects:
+            stats = [
+                ("🫂", "Students", sub['total_students']),
+                ("🕰️", "Classes", sub['total_classes']),
+            ]
+        def share_btn():
+            if st.button(f"Share Code: {sub['name']}", key=f"share_{sub['subject_code']}", icon=":material/share:"):
+                share_subject_dialog(sub['name'], sub['subject_code'])
+            st.space()
+
+        subject_card(
+            name = sub['name'],
+            code = sub['subject_code'],
+            section = sub['section'],
+            stats=stats,
+            footer_callback=share_btn
+        )
+    else:
+        st.info("NO SUBJECTS FOUND. CREATE ONE ABOVE")
+
 
 def teacher_tab_attendance_records():
     st.header('Attendance Records')
